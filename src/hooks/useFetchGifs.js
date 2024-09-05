28163d8e120a5dccba9b8d31b38f6696c1042bd5
mavadev <gianmarcovalentinc@gmail.com>
2024-09-05 12:37:10 -0500

    fix: correct path of getGifs.js

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/GetGifs';
+import { getGifs } from '../helpers/getGifs.js';
 
 export const useFetchGifs = (category) => {
 	const [gifs, setGifs] = useState([]);
